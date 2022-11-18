e1651452a84f2d5147fc463780a6da35bfd9a875
MattiaLongo03 <mattilongo2003@gmail.com>
2022-11-18 01:43:42 +0100

    commit

--- js/script.js
+++ js/script.js
@@ -1,168 +1,217 @@
-const app = new Vue ({
+ new Vue ({
     el: '#root',
     data: {
         contacts: [
             {
             name: 'Michele',
             avatar: '_1',
-            visible: true,
+            utenti: true,
             messages: [
             {
             date: '10/01/2020 15:30:55',
             message: 'Hai portato a spasso il cane?',
-            status: 'sent'
+            status: 'inviato'
             },
             {
             date: '10/01/2020 15:50:00',
             message: 'Ricordati di stendere i panni',
-            status: 'sent'
+            status: 'inviato'
             },
             {
             date: '10/01/2020 16:15:22',
             message: 'Tutto fatto!',
-            status: 'received'
+            status: 'ricevuto'
             }
             ],
             },
             {
             name: 'Fabio',
             avatar: '_2',
-            visible: true,
+            utenti: true,
             messages: [
             {
             date: '20/03/2020 16:30:00',
             message: 'Ciao come stai?',
-            status: 'sent'
+            status: 'inviato'
             },
             {
             date: '20/03/2020 16:30:55',
             message: 'Bene grazie! Stasera ci vediamo?',
-            status: 'received'
+            status: 'ricevuto'
             },
             {
             date: '20/03/2020 16:35:00',
             message: 'Mi piacerebbe ma devo andare a fare la spesa.',
-            status: 'sent'
+            status: 'inviato'
             }
             ],
             },
             {
             name: 'Samuele',
             avatar: '_3',
-            visible: true,
+            utenti: true,
             messages: [
             {
             date: '28/03/2020 10:10:40',
             message: 'La Marianna va in campagna',
-            status: 'received'
+            status: 'ricevuto'
             },
             {
             date: '28/03/2020 10:20:10',
             message: 'Sicuro di non aver sbagliato chat?',
-            status: 'sent'
+            status: 'inviato'
             },
             {
             date: '28/03/2020 16:15:22',
             message: 'Ah scusa!',
-            status: 'received'
+            status: 'ricevuto'
             }
             ],
             },
             {
             name: 'Alessandro B.',
             avatar: '_4',
-            visible: true,
+            utenti: true,
             messages: [
             {
             date: '10/01/2020 15:30:55',
             message: 'Lo sai che ha aperto una nuova pizzeria?',
-            status: 'sent'
+            status: 'inviato'
             },
             {
             date: '10/01/2020 15:50:00',
             message: 'Si, ma preferirei andare al cinema',
-            status: 'received'
+            status: 'ricevuto'
             }
             ],
             },
             {
             name: 'Alessandro L.',
             avatar: '_5',
-            visible: true,
+            utenti: true,
             messages: [
             {
             date: '10/01/2020 15:30:55',
             message: 'Ricordati di chiamare la nonna',
-            status: 'sent'
+            status: 'inviato'
             },
             {
             date: '10/01/2020 15:50:00',
             message: 'Va bene, stasera la sento',
-            status: 'received'
+            status: 'ricevuto'
             }
             ],
             },
             {
             name: 'Claudia',
             avatar: '_6',
-            visible: true,
+            utenti: true,
             messages: [
             {
             date: '10/01/2020 15:30:55',
             message: 'Ciao Claudia, hai novità?',
-            status: 'sent'
+            status: 'inviato'
             },
             {
             date: '10/01/2020 15:50:00',
             message: 'Non ancora',
-            status: 'received'
+            status: 'ricevuto'
             },
             {
             date: '10/01/2020 15:51:00',
             message: 'Nessuna nuova, buona nuova',
-            status: 'sent'
+            status: 'inviato'
             }
             ],
             },
             {
             name: 'Federico',
             avatar: '_7',
-            visible: true,
+            utenti: true,
             messages: [
             {
             date: '10/01/2020 15:30:55',
             message: 'Fai gli auguri a Martina che è il suo compleanno!',
-            status: 'sent'
+            status: 'inviato'
             },
             {
             date: '10/01/2020 15:50:00',
             message: 'Grazie per avermelo ricordato, le scrivo subito!',
-            status: 'received'
+            status: 'ricevuto'
             }
             ],
             },
             {
             name: 'Davide',
             avatar: '_8',
-            visible: true,
+            utenti: true,
             messages: [
             {
             date: '10/01/2020 15:30:55',
             message: 'Ciao, andiamo a mangiare la pizza stasera?',
-            status: 'received'
+            status: 'ricevuto'
             },
             {
             date: '10/01/2020 15:50:00',
             message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
-            status: 'sent'
+            status: 'inviato'
             },
             {
             date: '10/01/2020 15:51:00',
             message: 'OK!!',
-            status: 'received'
+            status: 'ricevuto'
             }
             ],
             }
-        ]
-    }
+        ],
+        chat: 0,
+        nuovo_messaggio: '',
+        ricerca: '',
+        nome_filtrato: '',
+    },
+    methods: {
+        selezioneChat(index){
+            this.chat = index;
+        },
+        invioMesaggio(){
+            if(this.nuovo_messaggio.trim()) {
+                let messaggioObj = {
+                    date: 'da vedere',
+                    message: this.nuovo_messaggio.trim(),
+                    status: 'inviato',
+                }
+                this.contacts[this.chat].messages.push(messaggioObj);
+                this.nuovo_messaggio = '';
+                setTimeout(this.receiveMsg, 1000);
+            }
+        },
+        ricezioneMesaggio(){
+            let messaggioObj = {
+                date: 'da vedere',
+                message: 'Ok',
+                status: 'ricevuto',
+            }
+            this.contacts[this.chat].messages.push(messaggioObj);
+        },
+        cercaContatto(){
+            for(let i=0; i<this.contacts.length; i++){
+                if(this.ricerca === this.contacts[i].name.slice(0, this.ricerca.length)){
+                    this.contacts[i].utenti = true
+                }
+                else{
+                    this.contacts[i].utenti = false
+                }
+            }
+        },
+        Filtro() {
+            this.contacts.forEach((ele, i) => {
+                if (!this.contacts[i].name.toLowerCase().includes(this.nome_filtrato.toLowerCase())){
+                    this.contacts[i].utenti = false;
+                } else {
+                    this.contacts[i].utenti = true
+                }
+            });
+        }
+    },
+    
 });
